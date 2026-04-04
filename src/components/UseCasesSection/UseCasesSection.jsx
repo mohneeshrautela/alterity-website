063101570bb70714ab99f87b5ec180f8bbf2351d
Mohneesh <mohneesh@gmail.com>
2026-04-04 10:55:00 +0530

    mobile view fix final

--- src/components/UseCasesSection/UseCasesSection.jsx
+++ src/components/UseCasesSection/UseCasesSection.jsx
@@ -59,6 +59,7 @@ function Card({ i, title, body, color, image, progress, range, targetScale }) {
         style={{
           backgroundColor: color,
           scale,
+          top: `calc(-5vh + ${i * 25}px)`,
         }}
       >
         {image ? (
